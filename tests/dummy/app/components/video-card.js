65d5f2f4228e286011e0b5ddd5edf3f7884cf45e
BrianSipple <bsipple57@gmail.com>
2016-08-25 00:20:33 -0700

    style polish for video card

--- tests/dummy/app/components/video-card.js
+++ tests/dummy/app/components/video-card.js
@@ -7,12 +7,14 @@ const { readOnly } = computed;
 
 export default Component.extend({
   layout,
-
+  tagName: 'section',
   classNames: ['c-card', 'c-video-card', 'o-widget-box', 'o-content-box', 'u-relative'],
+
   video: null,
 
   title: readOnly('video.title'),
-  description: readOnly('video.description') || '(No description)',
+  // description: readOnly('video.description') || '(No description)',
+  description: `I'll keep all my plans close to my chest. I'll wait here and see which way the wind will blow`.repeat(10).toUpperCase(),
   viewCount: readOnly('video.views') || 0,
   game: readOnly('video.game'),
   previewSrc: readOnly('video.preview'),
