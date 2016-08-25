feb774d462d24106670687b2e6a03bb22ef5a275
BrianSipple <bsipple57@gmail.com>
2016-08-25 01:14:11 -0700

    initial user model and adapter

--- addon/models/twitch-user.js
+++ addon/models/twitch-user.js
@@ -1,7 +1,20 @@
-import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import DS from 'ember-data';
+
+const { Model, hasMany, attr } = DS;
+
 
 export default Model.extend({
+  name: attr('string'),
+  createdAt: attr('date'),
+  updatedAt: attr('date'),
+  logo: attr('string'),
+  displayName: attr('string'),
+  email: attr('string'),
+  partnered: attr('boolean'),
+  bio: attr('string'),
+  notifications: attr(),
+
+  streamsFollowed: belongsTo('twitch-stream'),
+  videosFollowed: belongsTo('twitch-video')
 
 });
---
+++ tests/unit/adapters/twitch-user-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('adapter:twitch-user', 'Unit | Adapter | twitch user', {
+  // Specify the other units that are required for this test.
+  // needs: ['serializer:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let adapter = this.subject();
+  assert.ok(adapter);
+});
