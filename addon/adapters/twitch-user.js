616028314743c406d6ce8c7edbeacc9e0a5b41e9
BrianSipple <bsipple57@gmail.com>
2016-08-25 01:41:15 -0700

    clean urlForQuery

--- addon/adapters/twitch-user.js
+++ addon/adapters/twitch-user.js
@@ -1,16 +1,4 @@
 import DS from 'ember-data';
 
-const { JSONAPIAdapter } = DS;
-
-
-export default JSONAPIAdapter.extend({
-
-  urlForQuery(query, modelName) {
-    const url = this._super(...arguments);
-
-    return query.method === 'follows' ?
-    if (query.method === 'follows') {
-      return ``
-    }
-  }
+export default DS.JSONAPIAdapter.extend({
 });
